9d1d52e90ba437805f519b911134bda6813e0ef4
Jesse <jessewilliamson@fastmail.fm>
2020-11-01 08:36:14 -0800

    Use Turtle for CLI and Text instead of String

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Lib
-import Console.Options
-import Data.Version
+import Turtle
+import Data.Text
+
+data Command =
+      Encode Text
+    | Decode Text
+
+parser :: Parser Command
+parser
+    =
+        fmap Encode
+             (subcommand "encode" "Base64 encode a string"
+                (argText "string" "The string you want to encode"
+                )
+             )
+    <|> fmap Decode 
+             (subcommand "decode" "Decode a Base64 string"
+                (argText "string" "The string you want to decode")
+              )
 
 main :: IO ()
-main = defaultMain $ do
-    programName "hencode"
-    programVersion $ makeVersion [0,0,1]
-    programDescription "Encode and decode Base64"
-    flagA    <- flag $ FlagLong "decode" <> FlagShort 'd'
-    str <- argument "A base64 string" Right
-    action $ \toParam -> putStrLn $ if toParam flagA then decode (toParam str) else encode (toParam str)
+main = do
+    cmd <- options "Hencode" parser
+    case cmd of
+        Encode s -> putStrLn . unpack $ encode s
+        Decode s -> putStrLn . unpack $ decode s 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,19 @@
+
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib (encode, decode) where
 
 import Data.ByteString (ByteString)
-import Data.ByteString.Char8 (pack, unpack)
+import Data.ByteString.Char8 as C 
 import qualified Data.ByteString.Base64 as B64
+import Data.Text as T 
 
-handleError :: Either String ByteString -> String
-handleError (Left s) = s
-handleError (Right b) = unpack b
+handleError :: Either String ByteString -> Text 
+handleError (Left s) = T.pack s
+handleError (Right b) = T.pack $ C.unpack b
 
-encode :: String -> String
-encode = unpack . B64.encode . pack
+encode :: Text -> Text
+encode = T.pack . C.unpack . B64.encode . C.pack . T.unpack
 
-decode :: String -> String
-decode = handleError . B64.decode . pack
+decode :: Text -> Text
+decode = handleError . B64.decode . C.pack . T.unpack
